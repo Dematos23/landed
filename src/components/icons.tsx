77e13bea39dadfbfd8a0e61bfda3cb5dc156ad7e
Diego Matos <dematos23@gmail.com>
2025-08-14 20:33:19 +0000

    tu icono de google se ve mal en lugar de lucide-react usa react-icons pa

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -1,4 +1,7 @@
+"use client";
+
 import type { SVGProps } from "react";
+import { FcGoogle } from "react-icons/fc";
 
 export function Logo(props: SVGProps<SVGSVGElement>) {
   return (
@@ -17,19 +20,6 @@ export function Logo(props: SVGProps<SVGSVGElement>) {
   );
 }
 
-export function GoogleIcon(props: SVGProps<SVGSVGElement>) {
-  return (
-    <svg 
-      role="img" 
-      viewBox="0 0 24 24" 
-      xmlns="http://www.w3.org/2000/svg"
-      {...props}
-    >
-      <title>Google</title>
-      <path 
-        d="M12.48 10.92v3.28h7.84c-.24 1.84-.85 3.18-1.73 4.1-1.02 1.02-2.62 1.98-4.66 1.98-3.56 0-6.47-2.92-6.47-6.55s2.91-6.55 6.47-6.55c2.04 0 3.3.83 4.1 1.62l2.56-2.56c-1.62-1.5-3.8-2.62-6.66-2.62-5.45 0-9.84 4.4-9.84 9.85s4.39 9.85 9.84 9.85c5.03 0 8.3-3.56 8.3-8.5v-1.2H12.48z" 
-        fill="currentColor"
-      />
-    </svg>
-  )
+export function GoogleIcon(props: React.ComponentProps<"svg">) {
+  return <FcGoogle {...props} />;
 }
